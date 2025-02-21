fe916f99c6f0d8e4aefb619890250513a8862285
pdelaplana <patrick.delaplana@gmail.com>
2025-02-22 09:28:55 +1000

    Change output dir in config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), legacy()],
+  build: {
+    outDir: 'dist',
+  },
   test: {
     globals: true,
     environment: 'jsdom',
